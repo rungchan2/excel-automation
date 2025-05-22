9cedec8ec296800325abe0d7f16be4735634cd2c
heechan <leeh09077@gmail.com>
2025-05-22 20:27:47 +0900

    fix: robottxt 해결

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,4 @@
-import type { MetadataRoute } from "next"
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://classflow.mostercoop.co.kr/sitemap.xml",
-  }
+    sitemap: "https://classflow.monstercoop.co.kr/sitemap.xml",
+  };
 }
